8afa05eb7a5aacb5e3795f2321b58a7f64ba5c56
davidCarba <da.carballal@gmail.com>
2019-03-04 20:52:24 +0100

    Añadimos el get user profile

--- ejercicios/social-network/webserver/controllers/user/get-user-profile.js
+++ ejercicios/social-network/webserver/controllers/user/get-user-profile.js
@@ -1,7 +1,17 @@
 'use strict';
 
+const UserModel = require('../../../models/user-model');
+
 async function getUserProfile(req, res, next) {
-  return res.send(req.claims);
+  const { claims } = req;
+
+  try {
+    const userDataProfile = await UserModel.findOne({ uuid: claims.uuid }, '-_id -__v').lean();
+    console.log(userDataProfile);
+    return res.status(200).send(userDataProfile);
+  } catch (e) {
+    return res.status(404).send(e.message);
+  }
 }
 
 module.exports = getUserProfile;
