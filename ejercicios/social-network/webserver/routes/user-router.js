29a35112006db8eda269ac3e8ce99571a6568840
davidCarba <da.carballal@gmail.com>
2019-03-07 19:01:02 +0100

    created upload-user-avatar and the route

--- ejercicios/social-network/webserver/routes/user-router.js
+++ ejercicios/social-network/webserver/routes/user-router.js
@@ -1,13 +1,17 @@
 'use strict';
 
 const express = require('express');
+const multer = require('multer');
 const getUserProfile = require('../controllers/user/get-user-profile');
 const checkJwtToken = require('../controllers/session/check-jwt-token');
 const updateUserProfile = require('../controllers/user/update-user-profile');
+const uploadUserAvatar = require('../controllers/user/upload-user-avatar');
 
+const upload = multer();
 const router = express.Router();
 
 router.get('/user', checkJwtToken, getUserProfile);
 router.put('/user', checkJwtToken, updateUserProfile);
+router.post('/user/avatar', checkJwtToken, upload.single('avatar'), uploadUserAvatar);
 
 module.exports = router;
